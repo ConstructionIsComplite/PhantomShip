92c0341f0ca29a2f9fbc6c11e38c3e6db6fd7ffa
itarasov <t0ras.ilusha@gmail.com>
2025-04-13 00:08:22 +0300

    Added HealthBar

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
@@ -11,16 +13,25 @@ public class Health : MonoBehaviour
     [Header("Effects")]
     [SerializeField] GameObject deathEffect;
 
+    [Header("UI References")]
+    [SerializeField] Slider healthSlider;
+    [SerializeField] TMP_Text healthText;
+
     private int currentHealth;
+
+    public event System.Action<float> OnHealthChanged;
 
     void Start()
     {
         currentHealth = maxHealth;
+        UpdateUI();
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+        currentHealth = Mathf.Max(currentHealth, 0);
+        UpdateUI();
         if (currentHealth <= 0) Die();
     }
 
@@ -37,5 +48,22 @@ public class Health : MonoBehaviour
     public void Heal(int amount)
     {
         currentHealth = Mathf.Min(currentHealth + amount, maxHealth);
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        float healthPercentage = (float)currentHealth / maxHealth;
+
+        // Обновление Slider
+        if (healthSlider)
+            healthSlider.value = healthPercentage;
+
+        // Обновление Text
+        if (healthText)
+            healthText.text = $"HP: {currentHealth}/{maxHealth}";
+
+        // Вызов события
+        OnHealthChanged?.Invoke(healthPercentage);
     }
 }
